8a8fe7026c8a825e485d13dc2f66c27e3c3caae5
prostornet <prostornet@gmail.com>
2017-10-19 15:03:46 +0300

    add files for linux scripts 19.10 prostornet

---
+++ scripts/fly_m3n.5.sh
@@ -0,0 +1,306 @@
+#!/bin/bash
+# Set colors for  script.
+BLU1=$'\033[36m'
+NC1=$'\033[0m'
+RED='\033[31m'
+WHITE='\033[37m'
+NC='\033[0m'
+
+TMP_FILE1=$(mktemp)
+
+##################################
+###  Check for SU and BusyBox  ###
+##################################
+CHK_SU() {
+echo ""
+echo -e "${WHITE}. Проверка рут прав и наличие приложения Super SU:${NC}"
+echo  ""
+echo -e "Если в списке ниже вы видите ${RED}su${NC}, значит все впорядке, рут права предоставлены, приложение установлено."
+echo -e "Если в списке ниже отсутствует ${RED}su${NC}, необходимо выполните установку приложения ${RED}Super SU${NC} и обновите бинарный файл."
+echo ""
+adb shell "su -c ls /system/xbin"
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+###########################
+###  Install Supper SU  ###
+###########################
+INST_SU() {
+echo ""
+echo -e "${WHITE}. Установка приложения Super SU:${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+adb install -r files/su/SuperSU.apk >/dev/null 2>&1
+echo -e "${RED}!!! Установка завершина !!!${NC} Перезагрузка устройства ..."
+adb reboot
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+###############################
+###  Deleting chinese apps  ###
+###############################
+RMAPP_CHN() {
+echo ""
+echo -e "${WHITE}. Удаление китайского софта:${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+echo "Удаление приложений из /custom/3rd-party ..."
+echo "Удаление приложений Painter, Weather, Map, Reader, EBook, Sogou"
+
+while read line; do
+    adb uninstall ${line}
+    echo -e "${RED}!!! Удаление приложения${NC} ${WHITE}${line}${NC} ${RED}завершино !!!${NC}"
+done < files/sys_app/del.txt
+
+adb shell su -c mount -o remount,rw /system >/dev/null 2>&1
+adb shell su -c rm -r /system/app/Painter >/dev/null 2>&1
+adb shell su -c rm -r /system/app/Weather >/dev/null 2>&1
+#adb shell su -c rm -r /system/app/Map >/dev/null 2>&1
+adb shell su -c rm -r /system/app/Reader >/dev/null 2>&1
+adb shell su -c rm -r /system/priv-app/EBook >/dev/null 2>&1
+
+echo -e "${RED}!!! Удаление завершино !!!${NC} Перезагрузка устройства ..."
+adb reboot
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+###############################
+###  Disabled chinese apps  ###
+###############################
+PMAPP_CHN() {
+echo ""
+echo -e "${WHIRE}. Заморозка стокового софта:${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+echo "Заморозка системных приложений ..."
+
+while read line; do
+    adb shell su -c pm disable ${line}
+    echo -e "${RED}!!! Заморозка приложения${NC} ${WHITE}${line}${NC} ${RED}завершино !!!${NC}"
+done < files/sys_app/freeze.txt
+
+echo -e "${RED}!!! Заморозка завершина !!!${NC} Перезагрузка устройства (нет) ..."
+#adb reboot
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+####################################
+###  Translate chinese firmware  ###
+####################################
+INST_RUS() {
+echo ""
+echo -e "${WHITE}. Перевод китайской прошивки:${NC}"
+echo ""
+echo "Подсоединение к устройству ..."
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+echo "Копирование файлов в /tmp ..."
+echo ""
+adb push files/priv-app /data/local/tmp/priv-app #>/dev/null 2>&1
+adb push files/app /data/local/tmp/app #>/dev/null 2>&1
+adb push files/framework /data/local/tmp/framework #>/dev/null 2>&1
+adb push files/build.prop /data/local/tmp/ #>/dev/null 2>&1
+echo "Установка разрешений ..."
+echo ""
+adb shell "su -c chmod 644 /data/local/tmp/priv-app/*/*.apk" #>/dev/null 2>&1
+adb shell "su -c chmod 644 /data/local/tmp/app/*/*.apk" #>/dev/null 2>&1
+adb shell "su -c chmod 644 /data/local/tmp/framework/*/*.apk" #>/dev/null 2>&1
+adb shell "su -c chmod 644 /data/local/tmp/framework/*.apk" #>/dev/null 2>&1
+adb shell "su -c chmod 644 /data/local/tmp/build.prop" #>/dev/null 2>&1
+echo "Перенос приложений в /system ..."
+echo ""
+adb shell "su -c mount -o remount,rw /system" #>/dev/null 2>&1
+adb shell "su -c cp -r /data/local/tmp/priv-app/* /system/priv-app" #>/dev/null 2>&1
+adb shell "su -c cp -r /data/local/tmp/app/* /system/app" #>/dev/null 2>&1
+adb shell "su -c cp -r /data/local/tmp/framework/* /system/framework" #>/dev/null 2>&1
+adb shell "su -c cp -r /data/local/tmp/build.prop /system" #>/dev/null 2>&1
+echo "Очистка папки /tmp ..."
+echo ""
+adb shell "rm -r /data/local/tmp/priv-app" #>/dev/null 2>&1
+adb shell "rm -r /data/local/tmp/app" #>/dev/null 2>&1
+adb shell "rm -r /data/local/tmp/framework" #>/dev/null 2>&1
+adb shell "rm -r /data/local/tmp/build.prop" #>/dev/null 2>&1
+echo "!!! Перевод прошивки выполнен !!! Перезагрузка устройства ..."
+adb reboot >/dev/null 2>&1
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+#############################
+###  Checcking device ID  ###
+#############################
+CHK_ID() {
+echo ""
+echo -e "${WHITE}. Проверка ID устройства:${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+adb push files/dev_id/TST_ID /data/local/tmp/ >/dev/null 2>&1
+#adb shell su -c mount -o remount,rw /system >/dev/null 2>&1
+adb shell "su -c sh /data/local/tmp/TST_ID"
+adb shell rm -r /data/local/tmp/TST_ID >/dev/null 2>&1
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+#########################################################
+###  Change ID from chinese or international to beta  ###
+#########################################################
+#INT_ID() {
+#echo ""
+#echo -e "${WHITE}. Подмена ID устройства под международную прошивку:${NC}"
+#echo ""
+#adb kill-server >/dev/null 2>&1
+#adb start-server >/dev/null 2>&1
+#adb push files/dev_id/68151001 /data/local/tmp/ >/dev/null 2>&1
+#adb shell "su -c mount -o remount,rw /system" >/dev/null 2>&1
+#adb shell "su -c sh /data/local/tmp/68151001"
+#adb shell rm -r /data/local/tmp/68151001 >/dev/null 2>&1
+#adb reboot >/dev/null 2>&1
+#echo -e "${WHITE}...${NC}\n"
+#sh $0
+#}
+#########################################################
+###  Change ID from international or beta to chinese  ###
+#########################################################
+#CHN_ID() {
+#echo ""
+#echo -e "${WHITE}. Подмена ID устройства под китайскую прошивку:${NC}"
+#echo ""
+#adb kill-server >/dev/null 2>&1
+#adb start-server >/dev/null 2>&1
+#adb push files/dev_id/68101006 /data/local/tmp/ >/dev/null 2>&1
+#adb shell "su -c mount -o remount,rw /system" >/dev/null 2>&1
+#adb shell "su -c sh /data/local/tmp/68101006"
+#adb shell rm -r /data/local/tmp/68101006 >/dev/null 2>&1
+#adb reboot >/dev/null 2>&1
+#echo -e "${WHITE}...${NC}\n"
+#sh $0
+#}
+#########################################################
+###  Change ID from international or chinese to beta  ###
+#########################################################
+#CHN_ID() {
+#echo ""
+#echo -e "${WHITE}. Подмена ID устройства под китайскую прошивку:${NC}"
+#echo ""
+#adb kill-server >/dev/null 2>&1
+#adb start-server >/dev/null 2>&1
+#adb push files/dev_id/68101000 /data/local/tmp/ >/dev/null 2>&1
+#adb shell "su -c mount -o remount,rw /system" >/dev/null 2>&1
+#adb shell "su -c sh /data/local/tmp/68101000"
+#adb shell rm -r /data/local/tmp/68101000 >/dev/null 2>&1
+#adb reboot >/dev/null 2>&1
+#echo -e "${WHITE}...${NC}\n"
+#sh $0
+#}
+################################
+###  Install local user app  ###
+################################
+INST_APP() {
+echo ""
+echo -e "${WHITE}. Установка пользовательских приложений (папка files/user_app)${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+USRAPPS=(`ls -1 files/user_app/`)
+select i in "${USRAPPS[@]}"; do
+    break
+done
+echo -e "${BLU1}Установка приложения:${NC} ${WHITE}${i}${NC}"
+adb install -r files/user_app/${i} >/dev/null 2>&1
+echo -e "${RED}!!! Установка приложения${NC} ${WHITE}${i}${NC} ${RED}завершено !!!${NC}"
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+##############################
+###
+##############################
+UNINST_APP() {
+echo ""
+echo -e "${WHITE}. Удаление пользовательских приложений${NC}"
+echo ""
+adb kill-server >/dev/null 2>&1
+adb start-server >/dev/null 2>&1
+adb shell "su -c ls /data/data/" | grep -vE 'mediatek|meizu|android|flyme' > ${TMP_FILE1}
+dos2unix ${TMP_FILE1} >/dev/null 2>&1
+USR_APP=(`cat ${TMP_FILE1}`)
+select i in "${USR_APP[@]}"; do
+    break
+done
+echo -e "${BLU1}Удаление приложения:${NC} ${WHITE}${i}${NC}"
+adb uninstall ${i} >/dev/null 2>&1
+echo -e "${RED}!!! Удаление приложения${NC} ${WHITE}${i}${NC} ${RED}завершено !!!${NC}"
+echo -e "${WHITE}...${NC}\n"
+sh $0
+}
+
+
+############################ 
+### Main Menu for script ### 
+############################ 
+
+echo -e "${BLU1}Скрипт для MEIZU M3 Note & Flyme OS (автор ProstorNET)${NC}"
+
+ACT_1="Проверка прав суперпользователя"
+ACT_2="Установка SuperSU (ver.2.79)"
+ACT_3="Удаление китайского софта"
+ACT_4="Заморозка софта"
+ACT_5="Перевод прошивки"
+ACT_6="Проверка ID устройства"
+ACT_7="Подмена ID устройства под международную прошивку"
+ACT_8="Подмена ID устройства под китайскую прошивку"
+ACT_9="Подмена ID устройства под бета прошивку"
+ACT_10="Установка пользовательских приложений (папка files/user_app)"
+ACT_11="Удаление пользовательских приложений с телефона"
+ACT_12="Выход"
+
+PS3="${BLU1}Ваш выбор пользователь: ${NC1}"
+
+select CHOICES in "${ACT_1}" "${ACT_2}" "${ACT_3}" "${ACT_4}" "${ACT_5}" "${ACT_6}" "${ACT_7}" "${ACT_8}" "${ACT_9}" "${ACT_10}" "${ACT_11}" "${ACT_12}"; do
+    case ${CHOICES} in
+        "${ACT_1}")
+            CHK_SU
+            ;;
+        "${ACT_2}")
+            INST_SU
+            ;;
+        "${ACT_3}")
+            RMAPP_CHN
+            ;;
+        "${ACT_4}")
+            PMAPP_CHN
+            ;;
+        "${ACT_5}")
+            INST_RUS
+            ;;
+        "${ACT_6}")
+            CHK_ID
+            ;;
+        "${ACT_7}")
+            INT_ID
+            ;;
+        "${ACT_8}")
+            CHN_ID
+            ;;
+        "${ACT_9}")
+            BET_ID
+            ;;
+        "${ACT_10}")
+            INST_APP
+            ;;
+        "${ACT_11}")
+            UNINST_APP
+            ;;
+        "${ACT_12}")
+            rm -rf ${TMP_FILE1}
+            exit 0
+            ;;
+    esac
+    break
+done
+
+rm -rf ${TMP_FILE1}
+exit 0
